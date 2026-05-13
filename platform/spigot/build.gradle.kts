df7ea34345c7e1c79dfd4de844944841a012ad3e
宁东旭 <3318029085@qq.com>
2026-05-13 16:57:30 +0800

    feat: 添加 HamsterCurrency 依赖

--- platform/spigot/build.gradle.kts
+++ platform/spigot/build.gradle.kts
@@ -9,6 +9,7 @@ tasks {
 dependencies {
     implementation(project(":api"))
     compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("cn.hamster3:HamsterCurrency:2.2.0")
     compileOnly("me.clip:placeholderapi:2.10.4")
     compileOnly("org.serverct.ersha:AttributePlus:3.3.3.0")
     compileOnly("pers.neige.neigeitems:NeigeItems:1.21.62")
